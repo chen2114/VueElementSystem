61bd114b71df2b8b682d6bd30a945b19ea2d1474
13655079137 <731003808@qq.com>
2020-05-08 14:55:20 +0800

    删除

--- src/api/role.js
+++
@@ -1,7 +0,0 @@
-import { httpGet } from '@/utils/request'
-
-// 获取角色routes列表
-export const getRoleRoutesList = () => {
-  const url = '/getRoleRoutesList'
-  return httpGet(url)
-}
--- src/utils/validate.js
+++
@@ -1,8 +0,0 @@
-/**
- * @method isExternal 判断是否为外部链接
- * @param {string} path
- * @returns {Boolean}
- */
-export function isExternal (path) {
-  return /^(https?:|mailto:|tel:)/.test(path)
-}
